45ccb4f4c48b8fb3d3c70418deba78c7692f8e0d
Burak İskender <burak.iskendr@gmail.com>
2023-09-24 13:47:03 +0300

    :wrench: chore: add and configure vite absolute path

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,6 +1,8 @@
+const path = require('path')
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'airbnb',
     'prettier',
@@ -11,7 +13,22 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+        extensions: ['.js', '.jsx'],
+      },
+      alias: {
+        extensions: ['.jx', '.jsx'],
+        map: [
+          ['~', path.resolve(__dirname, './src/components')],
+          ['@', path.resolve(__dirname, './src')],
+        ],
+      },
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './src/components'),
+      },
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+    ],
+  },
 })
