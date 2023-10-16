c68b30962a7a82249ba8a81cbfa749cb8034255f
Burak İskender <burak.iskendr@gmail.com>
2023-10-16 14:29:49 +0300

    :sparkles: feat: add cors configuration

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -1,25 +1,33 @@
-module.exports = [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
+module.exports = ({ env }) => {
+  return [
+    "strapi::errors",
+    {
+      name: "strapi::security",
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            "connect-src": ["'self'", "https:"],
+            "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+            "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+            upgradeInsecureRequests: null,
+          },
         },
       },
     },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
-];
+    {
+      name: "strapi::cors",
+      config: {
+        headers: "*",
+        origin: env("CORS_WHITELIST").split(","),
+      },
+    },
+    "strapi::poweredBy",
+    "strapi::logger",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
+  ];
+};
